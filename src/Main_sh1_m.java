f518695544e16a4cef3d5c8a90522f83c16e325d
Filippov Roman <filiroman.tsu@gmail.com>
2012-11-03 23:39:36 +0400

    final version

--- src/Main_sh1_m.java
+++ src/Main_sh1_m.java
@@ -7,6 +7,6 @@ public class Main_sh1_m {
     public static void main(String[] args) throws IOException {
 
         Config c = LoadBalancer.loadConfig("config.json");
-        Node sh1_m = new Node(Integer.getInteger(c.shards.get(0).masterAddr.substring(c.shards.get(0).masterAddr.indexOf(":")+1)),true,c.shards.get(0).slavesAddr);
+        Node sh1_m = new Node(Integer.parseInt(c.shards.get(0).masterAddr.substring(c.shards.get(0).masterAddr.indexOf(":")+1)),true,c.shards.get(0).slavesAddr);
     }
 }
--- src/Main_sh2_m.java
+++ src/Main_sh2_m.java
@@ -7,6 +7,6 @@ public class Main_sh2_m {
     public static void main(String[] args) throws IOException {
 
         Config c = LoadBalancer.loadConfig("config.json");
-        Node sh1_m = new Node(Integer.getInteger(c.shards.get(1).masterAddr.substring(c.shards.get(1).masterAddr.indexOf(":")+1)),true,c.shards.get(1).slavesAddr);
+        Node sh1_m = new Node(Integer.parseInt(c.shards.get(1).masterAddr.substring(c.shards.get(1).masterAddr.indexOf(":")+1)),true,c.shards.get(1).slavesAddr);
     }
 }
