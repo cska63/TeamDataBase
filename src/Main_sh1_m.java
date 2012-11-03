ab8b55e0aba8351afccc46e19528f14bab66d28f
Filippov Roman <filiroman.tsu@gmail.com>
2012-11-03 23:22:03 +0400

    set master

--- src/Main_sh1_m.java
+++ src/Main_sh1_m.java
@@ -1,43 +1,12 @@
 import java.io.IOException;
 import java.util.ArrayList;
 
-/**
- * Created with IntelliJ IDEA.
- * User: vovan
- * Date: 11/1/12
- * Time: 11:31 PM
- * To change this template use File | Settings | File Templates.
- */
+
 public class Main_sh1_m {
 
-    private static final String DEFAULT_ADDRESS = "127.0.0.1:8080";
-
     public static void main(String[] args) throws IOException {
-//        Node master1=new Node(2122,true,2123,2124);
-//        Node slave11=new Node(2123,false);
-//        Node slave12=new Node(2124,false);
-        ArrayList<String> addr1 = new ArrayList<String>();
-        addr1.add("127.0.0.1:2223");
-        addr1.add("127.0.0.1:2224");
-//
-//        Node master2=new Node(2222,true,2223,2224);
-//        Node slave21=new Node(2223,false);
-//        Node slave22=new Node(2224,false);
-        ArrayList<String> addr2 = new ArrayList<String>();
-        addr2.add("127.0.0.1:2123");
-        addr2.add("127.0.0.1:2124");
-//
-        ArrayList<NodeAddr> ar = new ArrayList<NodeAddr>();
-        ar.add(new NodeAddr("127.0.0.1:2222",addr1));
-        ar.add(new NodeAddr("127.0.0.1:2122",addr2));
-//
-//
-        LoadBalancer loadBalancer=new LoadBalancer(8080, ar);
-        int portOfLoadBalancer=8080;
-        MyHttpClient m = new MyHttpClient(8888);
-        while (true) {
-            String q = m.enterCommand();
-            System.out.println(LoadBalancer.doQuery(q, DEFAULT_ADDRESS));
-        }
+
+        Config c = LoadBalancer.loadConfig("config.json");
+        Node sh1_m = new Node(Integer.getInteger(c.shards.get(0).masterAddr.substring(c.shards.get(0).masterAddr.indexOf(":")+1)),true,c.shards.get(0).slavesAddr);
     }
 }
--- src/Main_sh1_s2.java
+++ src/Main_sh1_s2.java
@@ -1,43 +1,14 @@
 import java.io.IOException;
 import java.util.ArrayList;
 
-/**
- * Created with IntelliJ IDEA.
- * User: vovan
- * Date: 11/1/12
- * Time: 11:31 PM
- * To change this template use File | Settings | File Templates.
- */
+
 public class Main_sh1_s2 {
 
-    private static final String DEFAULT_ADDRESS = "127.0.0.1:8080";
-
     public static void main(String[] args) throws IOException {
-//        Node master1=new Node(2122,true,2123,2124);
-//        Node slave11=new Node(2123,false);
-//        Node slave12=new Node(2124,false);
-        ArrayList<String> addr1 = new ArrayList<String>();
-        addr1.add("127.0.0.1:2223");
-        addr1.add("127.0.0.1:2224");
-//
-//        Node master2=new Node(2222,true,2223,2224);
-//        Node slave21=new Node(2223,false);
-//        Node slave22=new Node(2224,false);
-        ArrayList<String> addr2 = new ArrayList<String>();
-        addr2.add("127.0.0.1:2123");
-        addr2.add("127.0.0.1:2124");
-//
-        ArrayList<NodeAddr> ar = new ArrayList<NodeAddr>();
-        ar.add(new NodeAddr("127.0.0.1:2222",addr1));
-        ar.add(new NodeAddr("127.0.0.1:2122",addr2));
-//
-//
-        LoadBalancer loadBalancer=new LoadBalancer(8080, ar);
-        int portOfLoadBalancer=8080;
-        MyHttpClient m = new MyHttpClient(8888);
-        while (true) {
-            String q = m.enterCommand();
-            System.out.println(LoadBalancer.doQuery(q, DEFAULT_ADDRESS));
-        }
+
+        Config c = LoadBalancer.loadConfig("config.json");
+        ArrayList<String> newl = new ArrayList<String>(c.shards.get(0).slavesAddr.subList(1,c.shards.get(0).slavesAddr.size()-1));
+        newl.add(c.shards.get(0).masterAddr);
+        Node sh1_m = new Node(Integer.getInteger(c.shards.get(0).slavesAddr.get(1).substring(c.shards.get(0).slavesAddr.get(1).indexOf(":")+1)),false,newl);
     }
 }
