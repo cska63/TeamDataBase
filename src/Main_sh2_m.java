bfb02aad8ec2b9129799a2f48028860cb23d018a
Filippov Roman <filiroman.tsu@gmail.com>
2012-11-29 15:48:38 +0400

    final version

--- src/Main_sh2_m.java
+++ src/Main_sh2_m.java
@@ -7,7 +7,7 @@ public class Main_sh2_m {
     public static void main(String[] args) throws IOException {
 
         Config c = LoadBalancer.loadConfig("config.json");
-        int port = LoadBalancer.cutPortFromAddress(c.shards.get(1).masterAddr);
+        int port = LoadBalancer.cutPortFromAddress(c.shards.get(1).masterAddr); //
         new Node(port,true,c.shards.get(1).slavesAddr);
     }
 }
